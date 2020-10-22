d52e15b8cdc6fb4711bcf5072e6ae4d4c3bdb800
Christoph Steinkellner <nulpe@gmx.at>
2020-10-22 19:07:44 +0200

    Init

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+  /// <summary>
+  /// 1 - The speed of the ship
+  /// </summary>
+  public Vector2 speed = new Vector2(50, 50);
+  public bool flipOnShoot = false;
+  public Vector3 defaultScale = new Vector3(0, 0, 0);
+  private Vector3 flipScale = new Vector3(0, 0, 0);
+  private float flipTimer = 0f;
+
+  // 2 - Store the movement and the component
+  private Vector2 movement;
+  private Rigidbody2D rigidbodyComponent;
+
+  private void Awake()
+  {
+    flipScale = defaultScale;
+    flipScale.x *= -1;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // 3 - Retrieve axis information
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
+
+    // 4 - Movement per direction
+    movement = new Vector2(
+      speed.x * inputX,
+      speed.y * inputY
+    );
+
+    if (flipTimer > 0)
+    {
+      flipTimer -= Time.deltaTime;
+    }
+
+    // 5 - Shooting
+    bool shoot = Input.GetButton( "Fire1");
+    shoot |= Input.GetButton("Fire2");
+    // Careful: For Mac users, ctrl + arrow is a bad idea
+
+    if (shoot)
+    {
+      WeaponScript weapon = GetComponent<WeaponScript>();
+      if (weapon != null)
+      {
+        // Flip the image on the x axis
+        if (flipOnShoot)
+        {
+          flipTimer = 0.5f;
+          transform.localScale = flipScale;
+        }
+        // false because the player is not an enemy
+        weapon.Attack(false);
+      }
+    }
+    if (flipTimer < 0)
+    {
+      transform.localScale = defaultScale;
+      flipTimer = 0;
+    }
+  }
+
+  void FixedUpdate()
+  {
+      // 5 - Get the component and store the reference
+      if (rigidbodyComponent == null) {
+          rigidbodyComponent = GetComponent<Rigidbody2D>();
+      } 
+
+      // 6 - Move the game object
+      rigidbodyComponent.velocity = movement;
+  }
+}
---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Parallax scrolling script that should be assigned to a layer
+/// </summary>
+public class ScrollingScript : MonoBehaviour
+{
+  /// <summary>
+  /// Scrolling speed
+  /// </summary>
+  public Vector2 speed = new Vector2(2, 2);
+
+  /// <summary>
+  /// Moving direction
+  /// </summary>
+  public Vector2 direction = new Vector2(-1, 0);
+
+  /// <summary>
+  /// Movement should be applied to camera
+  /// </summary>
+  public bool isLinkedToCamera = false;
+
+  public bool isLooping = false;
+  
+  private List<SpriteRenderer> backgroundPart;
+
+  private void Start()
+  {
+    if (isLooping)
+    {
+      backgroundPart = new List<SpriteRenderer>();
+
+      for(int i = 0; i < transform.childCount; i++)
+      {
+        Transform child = transform.GetChild(i);
+        SpriteRenderer r = child.GetComponent<SpriteRenderer>();
+
+        if (r != null)
+        {
+          backgroundPart.Add(r);
+        }
+      }
+
+      // Sort by position.
+      // Note: Get the children from left to right.
+      // We would need to add a few conditions to handle
+      // all the possible scrolling directions.
+      backgroundPart = backgroundPart.OrderBy(
+        t => t.transform.position.x
+      ).ToList();
+    }
+  }
+
+  void Update()
+  {
+    // Movement
+    Vector3 movement = new Vector3(
+      speed.x * direction.x,
+      speed.y * direction.y,
+      0
+    );
+
+    movement *= Time.deltaTime;
+    transform.Translate(movement);
+
+    // Move the camera
+    if (isLinkedToCamera)
+    {
+      Camera.main.transform.Translate(movement);
+    }
+
+    if (isLooping)
+    {
+      SpriteRenderer firstChild = backgroundPart.FirstOrDefault();
+      if (firstChild.transform.position.x < Camera.main.transform.position.x)
+      {
+        if (firstChild.IsVisibleFrom(Camera.main) == false)
+        {
+          SpriteRenderer lastChild = backgroundPart.LastOrDefault();
+          Vector3 lastPosition = lastChild.transform.position;
+          Vector3 lastSize = (lastChild.bounds.max - lastChild.bounds.min);
+
+          // Set the position of the recyled one to be AFTER
+          // the last child.
+          // Note: Only work for horizontal scrolling currently.
+          firstChild.transform.position = new Vector3(lastPosition.x + lastSize.x, firstChild.transform.position.y, firstChild.transform.position.z);
+
+          // Set the recycled child to the last position
+          // of the backgroundPart list.
+          backgroundPart.Remove(firstChild);
+          backgroundPart.Add(firstChild);
+        }
+      }
+    }
+  }
+}
